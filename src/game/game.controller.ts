a45e8809611586c2eb0b5a22a4174ea5b4fc4479
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-19 14:30:04 +0200

    feat: implement use GameOfLife class in game service

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { GameService } from './game.service';
+import { Board } from '../utils/GameOfLife';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private readonly gameService: GameService) {}
+
+  @Get('/')
+  getResult(@Body() board: Board): Board {
+    return this.gameService.getResult(board);
+  }
+}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from '../utils/GameOfLife';
+import { GameOfLife } from '../utils/GameOfLife';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  getResult(board: Board): Board {
+    return new GameOfLife(board).tick().getState();
+  }
+}
